5959aa95b70bcfd243619ffb56fbd0668c24e214
SashaAndSeals <146691256+SashaAndSeals@users.noreply.github.com>
2023-10-02 17:39:50 -0700

    Update scratch2sphero.py

--- scratch2sphero.py
+++ scratch2sphero.py
@@ -4,7 +4,6 @@ from scratchcloud import CloudClient, CloudChange
 from spherov2 import scanner 
 from spherov2.sphero_edu import SpheroEduAPI,EventType
 from spherov2.types import Color
-
 chars = r'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+-*/()[]{}.,:;?!@#$%^&<>\|/`~=_ "'+"'"
 def decode(intcode):
     code=str(intcode)
@@ -18,30 +17,32 @@ def motor(droid,right,left,time):
     droid.raw_motor(right,left,time) 
 def pixel(droid,x,y,r,g,b):
     droid.set_matrix_pixel(x,y,Color(r,g,b))  
-print("Looking for sphero...")
-toy = scanner.find_toy()
-print("Connecting to sphero...")
-with SpheroEduAPI(toy) as droid:
-    print("Connected to sphero!")
-    client = CloudClient(input("Username:"),input("Project ID:"))
-    @client.event
-    async def on_connect():
-        print('Connected to scratch!')
+def main():
+    print("Looking for sphero...")
+    toy = scanner.find_toy()
+    print("Connecting to sphero...")
+    with SpheroEduAPI(toy) as droid:
+        print("Connected to sphero!")
+        client = CloudClient(input("Username:"),input("Project ID:"))
+        @client.event
+        async def on_connect():
+            print('Connected to scratch!')
 
-    @client.event
-    async def on_disconnect():
-        print('Disconnected from scratch!')
+        @client.event
+        async def on_disconnect():
+            print('Disconnected from scratch!')
 
-    @client.event
-    async def on_message(cloud: CloudChange):
-        if cloud.name=="SEND":
-            msg=decode(cloud.value)
-            print(msg)
-            if msg.startswith("motor("):
-                msg=msg.replace("motor(","").replace(")","").split(",")
-                motor(droid,int(msg[0]),int(msg[1]),int(msg[2]))
-            elif msg.startswith("pixel("):
-                msg=msg.replace("pixel(","").replace(")","").split(",")
-                pixel(droid,int(msg[0]),int(msg[1]),int(msg[2]),int(msg[3]),int(msg[4]))
-    client.run(input("Password:"))
-       
+        @client.event
+        async def on_message(cloud: CloudChange):
+            if cloud.name=="SEND":
+                msg=decode(cloud.value)
+                print(msg)
+                if msg.startswith("motor("):
+                    msg=msg.replace("motor(","").replace(")","").split(",")
+                    motor(droid,int(msg[0]),int(msg[1]),int(msg[2]))
+                elif msg.startswith("pixel("):
+                    msg=msg.replace("pixel(","").replace(")","").split(",")
+                    pixel(droid,int(msg[0]),int(msg[1]),int(msg[2]),int(msg[3]),int(msg[4]))
+        client.run(input("Password:"))
+if __name__ == "__main__":
+    main()
