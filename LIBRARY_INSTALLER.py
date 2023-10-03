cfb1b040fe7a68569cbd020ef9ef63b67d14eb83
SashaAndSeals <146691256+SashaAndSeals@users.noreply.github.com>
2023-10-02 17:33:00 -0700

    Update LIBRARY_INSTALLER.py

--- LIBRARY_INSTALLER.py
+++ LIBRARY_INSTALLER.py
@@ -2,5 +2,3 @@ import os
 os.system("pip install spherov2")
 os.system("pip install bleak")
 os.system("pip install scratchcloud")
-os.system("pip install threading")
-os.system("pip install time")   
